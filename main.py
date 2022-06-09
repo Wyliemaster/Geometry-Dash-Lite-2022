979a71593a733ae2410a54fa6511239adc2742d4
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-06-09 22:19:11 +0100

    Patches

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+
+with open("patches.json", "r") as f:
+    patch_json = json.loads(f.read())
+
+
+with open(patch_json["input_file"], "rb") as f:
+    b = bytearray(f.read())
+
+for x in patch_json["patches"]:
+    for idx, item in enumerate(x["patches"]):
+        b[int(x["offset"], 16) + idx] = int(item, 16)
+
+with open("libcocos2dcpp.so", "wb") as f:
+    f.write(b)
