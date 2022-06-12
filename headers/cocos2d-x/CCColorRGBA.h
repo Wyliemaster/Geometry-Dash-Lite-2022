a39f4f22dc38c601a9f59e6157ca19f1f6e84f7f
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-06-12 18:09:53 +0100

    CCColorRGBA

---
+++ headers/cocos2d-x/CCColorRGBA.h
@@ -0,0 +1,10 @@
+struct cocos2d::CCLayerRGBA : public cocos2d::CCLayer, public cocos2d::CCRGBAProtocol
+{
+public:
+    GLubyte m_uDisplayedOpacity;
+    GLubyte m_uOpacity;
+    cocos2d::ccColor3B m_cDisplayedColour;
+    cocos2d::ccColor3B m_cColour;
+    bool m_bCascadeOpacity;
+    bool m_bCascadeColour;
+};
