4b5e781b487d6f83abc3b0240437b7ee370ade07
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-06-12 17:59:34 +0100

    CCLayer

---
+++ headers/cocos2d-x/CCLayer.h
@@ -0,0 +1,20 @@
+#ifndef __CCLAYER_H__
+#define __CCLAYER_H__
+
+struct cocos2d::CCLayer : public cocos2d::CCNode, public cocos2d::CCTouchDelegate, public cocos2d::CCAccelerometerDelegate, public cocos2d::CCKeypadDelegate, public cocos2d::CCKeyboardDelegate, public cocos2d::CCMouseDelegate
+{
+public:
+    bool m_bTouchEnabled;
+    bool m_bAccelerometerEnabled;
+    bool m_bKeypadEnabled;
+    bool m_bKeyboardEnabled;
+    bool m_bMouseEnabled;
+    cocos2d::CCScriptHandlerEntry *m_pTouchHandler;
+    cocos2d::CCScriptHandlerEntry *m_pKeypadHandler;
+    cocos2d::CCScriptHandlerEntry *m_pAccelerateHandler;
+    int m_nTouchPriority;
+    int m_nTouchMode;
+};
+
+
+#endif
