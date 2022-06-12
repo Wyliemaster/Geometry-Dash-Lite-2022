31f034624436b7372cec9b2d05e26f55b0ee770a
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2022-06-12 18:14:50 +0100

    colour

---
+++ headers/cocos2d-x/CCLayerColor.h
@@ -0,0 +1,7 @@
+struct cocos2d::CCLayerColor : public cocos2d::CCLayerRGBA, public cocos2d::CCBlendProtocol
+{
+public:
+    cocos2d::ccVertex2F m_pSquareVertices[4];
+    cocos2d::ccColor4F m_pSquareColors[4];
+    cocos2d::ccBlendFunc m_tBlendFunc;
+};
